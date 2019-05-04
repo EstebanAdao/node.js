e87292e048081e5b5aabc10ade40725fa2807e50
Esteban Mondragon <esteban.perea@webmotors.com.br>
2019-05-04 19:26:14 -0300

    Commom

---
+++ modules-ex1/server.js
@@ -0,0 +1,14 @@
+//const operations = require('./operations')
+
+//console.log(operations.var1)
+//console.log(operations.var2)
+
+//const funcoes = require('./funcoes')
+//const resultado = funcoes(4)
+//console.log(resultado.msg, resultado.value)
+
+const funcoes2 = require('./funcoes2')
+const add = funcoes2.adicao(5,8)
+const sub = funcoes2.subtracao(10,7)
+
+console.log(add,	sub);
