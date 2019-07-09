4a6a7db918d529d75ef31e5afcaec67a2e0dbb9e
Esteban Perea <mondragon1669@gmail.com>
2019-07-09 11:39:35 -0300

    Mysql

--- app/app.js
+++ app/app.js
@@ -3,32 +3,53 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var expressNunjucks = require('express-nunjucks');
+var bodyParser = require('body-parser')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/musicas')
+var methodOverride = require('method-override');
+
+
+//var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'njk');
+
+var njk = expressNunjucks(app);
+
+app.use(bodyParser.urlencoded())
+app.use(methodOverride((req, res) => {
+  console.log('req', req.body)
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    console.log('Entrei')
+    var method = req.body._method
+    delete req.body._method
+    return method
+  }
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,8 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index', {
+    title: 'Express'
+  });
+});
+
+router.get('/tabuada/:valor', (req, res, next) => {
+  //res.render('index', { title: 'Express' });
+  var valor = req.params.valor;
+  var resultado = (valor % 2) == 0 ? valor : false;
+  res.render('tabuada', {
+    numero: resultado
+  })
 });
 
 module.exports = router;
